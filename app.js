9a7dfab8c30e2cafd197dbb1f5ef9ad6b5c6f137
NachiketE <nachiketerlekar@gmail.com>
2024-03-18 15:46:55 -0700

    Trying out NYT api of most popular articles

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const apiKey = 'gckYpXqcXTIGTPgFAZdDMkKSwKPAD1a5'; 
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Homepage' }); // Assuming you'll have an EJS template named 'index.ejs'
+    res.render('index', { title: 'Homepage' }); 
   });
 
-  app.use(express.static('public'));
+app.use(express.static('public'));
+
+
+// Define a route to fetch data from the API
+app.get('/articles', async (req, res) => {
+    try {
+        const response = await axios.get('https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json', {
+            params: {
+                'api-key': apiKey
+            }
+        });
+
+        const articles = response.data.results;
+        res.render('index', { articles }); 
+
+        console.log(articles)
+    } catch (error) {
+        console.error('Error fetching articles:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 
 const PORT = process.env.PORT || 3000;
