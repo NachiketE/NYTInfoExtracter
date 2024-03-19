609f4700341809154766c928c666ad73bd7884fa
NachiketE <nachiketerlekar@gmail.com>
2024-03-18 17:33:17 -0700

    Top stories API done

--- app.js
+++ app.js
@@ -22,11 +22,29 @@ app.get('/articles', async (req, res) => {
         });
 
         const articles = response.data.results;
-        res.render('index', { articles }); 
+        res.render('mostpopular', { articles }); 
 
         console.log(articles)
     } catch (error) {
         console.error('Error fetching articles:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+app.get('/topstories/:section', async (req, res) => {
+    const section = req.params.section;
+    try {
+        const response = await axios.get(`https://api.nytimes.com/svc/topstories/v2/${section}.json`, {
+            params: {
+                'api-key': apiKey
+            }
+        });
+
+        const articles = response.data.results;
+        res.render('topstories', { section, articles });
+    } catch (error) {
+        console.error('Error fetching top stories:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
