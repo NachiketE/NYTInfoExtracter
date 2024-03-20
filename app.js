9125fe5eb0b0b730f898b4f2eed29421a620a121
NachiketE <nachiketerlekar@gmail.com>
2024-03-19 17:17:35 -0700

    Realtime newswire /content/section-list.json  API added

--- app.js
+++ app.js
@@ -58,6 +58,9 @@ mostViewed(app, apiKey);
 const realtimeNewswire = require('./routes/realtimeNewswire');
 realtimeNewswire(app, apiKey);
 
+const sectionList = require('./routes/newswireSectionList');
+sectionList(app, apiKey);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ routes/newswireSectionList.js
@@ -0,0 +1,21 @@
+// timesNewswire.js
+
+const axios = require('axios');
+
+module.exports = (app, apiKey) => {
+    app.get('/section-list', async (req, res) => {
+        try {
+            const response = await axios.get('https://api.nytimes.com/svc/news/v3/content/section-list.json', {
+                params: {
+                    'api-key': apiKey
+                }
+            });
+
+            const sections = response.data.results;
+            res.render('newswireSectionList', { sections });
+        } catch (error) {
+            console.error('Error fetching section list:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
+    });
+};
